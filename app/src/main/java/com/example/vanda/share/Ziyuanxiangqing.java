c312eb7082a2cc73074b503563a7bf9abfa7fb18
Vanda <978863794@qq.com>
2018-10-29 15:38:01 +0800

    first

--- app/src/main/java/com/example/vanda/share/Ziyuanxiangqing.java
+++ app/src/main/java/com/example/vanda/share/Ziyuanxiangqing.java
@@ -3,7 +3,7 @@ package com.example.vanda.share;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ziyuanxiangqing extends AppCompatActivity {
+public class Ziyuanxiangqing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
