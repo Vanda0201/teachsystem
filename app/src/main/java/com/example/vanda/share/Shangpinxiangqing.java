9f481936c261b451e1a864ac4fbd564baca4f17a
Vanda <978863794@qq.com>
2018-10-29 17:13:31 +0800

    first

---
+++ app/src/main/java/com/example/vanda/share/Shangpinxiangqing.java
@@ -0,0 +1,13 @@
+package com.example.vanda.share;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shangpinxiangqing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shangpinxiangqing);
+    }
+}
