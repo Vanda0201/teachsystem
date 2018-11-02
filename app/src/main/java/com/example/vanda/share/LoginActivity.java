40c9373a8fbec22f14d0ae665dfb0ca06f2f78a0
Vanda <978863794@qq.com>
2018-11-02 19:07:46 +0800

    layout first_test

--- app/src/main/java/com/example/vanda/share/LoginActivity.java
+++ app/src/main/java/com/example/vanda/share/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.vanda.share;
 
         import android.content.Intent;
+        import android.content.SharedPreferences;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.View;
+        import android.widget.Button;
         import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,5 +23,14 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        Button loginData =(Button) findViewById(R.id.login_button);
+        loginData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("user",MODE_PRIVATE).edit();
+                editor.putString("user_name","Logged");
+                editor.apply();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vanda/share/RegisterActivity.java
+++ app/src/main/java/com/example/vanda/share/RegisterActivity.java
@@ -1,7 +1,10 @@
 package com.example.vanda.share;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        Button loginData =(Button) findViewById(R.id.register_button);
+        loginData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("user",MODE_PRIVATE).edit();
+                editor.putString("user_name","Logged");
+                editor.apply();
+            }
+        });
     }
 }
