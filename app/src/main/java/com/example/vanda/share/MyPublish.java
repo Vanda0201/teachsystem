b1990d665c11b7d740c446a314dfb1dfd1ad2491
Vanda <978863794@qq.com>
2018-11-04 13:04:38 +0800

    Layout of every page has done

--- app/src/main/java/com/example/vanda/share/MyPublish.java
+++ app/src/main/java/com/example/vanda/share/MyPublish.java
@@ -3,11 +3,11 @@ package com.example.vanda.share;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CollectionActivity extends AppCompatActivity {
+public class MyPublish extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_collection);
+        setContentView(R.layout.activity_my_publish);
     }
 }
