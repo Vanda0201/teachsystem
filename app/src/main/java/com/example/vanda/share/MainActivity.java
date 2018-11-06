2e30bd85301e660f3f4822a8371ce20d9b2a821d
Vanda <978863794@qq.com>
2018-11-06 22:22:49 +0800

    Last

--- app/src/main/java/com/example/vanda/share/MainActivity.java
+++ app/src/main/java/com/example/vanda/share/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vanda.share;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,11 +9,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.ResourceCursorTreeAdapter;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import static com.example.vanda.share.R.*;
+import static com.example.vanda.share.R.animator.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,15 +31,15 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_shouye:
+                case id.navigation_shouye:
                    replaceFragment(new FirstFragment());
                     // mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_shangcheng:
+                case id.navigation_shangcheng:
                   replaceFragment(new SecondFragment());
                     //mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_wode:
+                case id.navigation_wode:
                     replaceFragment(new ThirdFragment());
                     // mTextMessage.setText(R.string.title_notifications);
                     return true;
@@ -39,29 +47,34 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(id.content, fragment);
+        transaction.commit();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //透明状态栏
-        //ImmersiveStatusbar.getInstance().Immersive(getWindow(),getActionBar());
-        // mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        setContentView(layout.activity_main);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         //appy运行指定首画面
-        navigation.setSelectedItemId(R.id.navigation_shouye);
+        navigation.setSelectedItemId(id.navigation_shouye);
     }
-
-    private void replaceFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.content, fragment);
-        transaction.commit();
-    }
-
-
-
+        //悦享头条
+//        @SuppressLint("ResourceType")
+//        public View onCreateView(LayoutInflater inflater,
+//                                 ViewGroup container, Bundle savedInstanceState) {
+//            // TODO Auto-generated method stub
+//            View view = inflater.inflate(layout.frgment_first,container,false);
+//            ViewFlipper flipper = (ViewFlipper) view.findViewById(id.vf);
+//            flipper.setInAnimation(this.getBaseContext(),right_in);
+//            flipper.setOutAnimation(this.getBaseContext(),right_out);
+//            flipper.startFlipping();
+//            return view;
+//   }
 
 
 }
