22cffdbf8c4818ac021d1ac1fffbb5540a868da7
Vanda <978863794@qq.com>
2018-11-15 21:36:32 +0800

    Last

--- app/src/main/java/com/example/vanda/share/CertificateActivity.java
+++ app/src/main/java/com/example/vanda/share/CertificateActivity.java
@@ -1,17 +1,9 @@
 package com.example.vanda.share;
 
-import android.content.Intent;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class CertificateActivity extends AppCompatActivity {
 
