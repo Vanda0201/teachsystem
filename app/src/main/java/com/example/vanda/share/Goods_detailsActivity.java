150a137fb074722cc0c125f0251b97c5563dd78b
KB248 <1434821185@qq.com>
2018-11-20 09:50:22 +0800

    Merge branch 'master' of https://github.com/HSBeyond/shareapp
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/vanda/share/Goods_detailsActivity.java
+++ app/src/main/java/com/example/vanda/share/Goods_detailsActivity.java
@@ -1,8 +1,11 @@
 package com.example.vanda.share;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Goods_detailsActivity extends AppCompatActivity {
@@ -13,11 +16,43 @@ public class Goods_detailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_goods_details);
 //        返回键
         ImageView mypublishback = (ImageView) findViewById(R.id.title_back);
+        Button buy = (Button)findViewById(R.id.download_buy);
         mypublishback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                duihuakuang();
+            }
+            private  void duihuakuang(){
+                AlertDialog.Builder bb = new AlertDialog.Builder(Goods_detailsActivity.this);
+
+                bb.setPositiveButton("确定购买", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                bb.setNegativeButton("我再看看", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+
+                bb.setMessage("你确定要花费9.9元购买该商品吗？");
+                bb.setTitle("购买提示");
+                bb.show();
+            }
+
+        });
+
+
     }
 }
