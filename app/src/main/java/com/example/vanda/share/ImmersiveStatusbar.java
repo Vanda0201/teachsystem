79d6ad8d5b9ec92895a91ae986c4e8a44b222fa8
Vanda <978863794@qq.com>
2018-12-06 21:12:24 +0800

    last

--- app/src/main/java/com/example/vanda/share/ImmersiveStatusbar.java
+++ app/src/main/java/com/example/vanda/share/ImmersiveStatusbar.java
@@ -19,7 +19,6 @@ public class ImmersiveStatusbar {
 
     }
 
-    // TODO: 2017/4/17 单例模式
     public static ImmersiveStatusbar getInstance() {
 
         if (immersiveStatusbar == null) {
@@ -44,7 +43,7 @@ public class ImmersiveStatusbar {
         if (Build.VERSION.SDK_INT >= 21) {
 
             View view = window.getDecorView();
-            // TODO: 2017/4/13 两个FLAG一起使用表示会让应用的主体内容占用系统状态栏的时空间
+            // 两个FLAG一起使用表示会让应用的主体内容占用系统状态栏的时空间
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             view.setSystemUiVisibility(option);
             // 将状态栏设置成透明色
